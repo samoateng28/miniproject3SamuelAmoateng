e98ba7fb49c72a3e2c2d5ddf8f7b6ae22b96aa48
Samoateng28 <s_amoateng@mail.fhsu.edu>
2025-10-18 19:02:40 +0000

    Started tutorial

--- main.py
+++
@@ -1,5 +0,0 @@
-# INF601 - Advanced Programming in Python
-# Samuel Amoateng
-# Mini Project 3
-
-# (5/5 points) Initial comments with your name, class and project at the top of your .py file.  -- Done
