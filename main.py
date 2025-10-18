c726f55d0fc4fef33d9a0bb86eaeaa1e86882aed
Samoateng28 <s_amoateng@mail.fhsu.edu>
2025-10-18 17:18:14 +0000

    Intial comments with name , class and project done.

---
+++ main.py
@@ -0,0 +1,5 @@
+# INF601 - Advanced Programming in Python
+# Samuel Amoateng
+# Mini Project 3
+
+# (5/5 points) Initial comments with your name, class and project at the top of your .py file.  -- Done
